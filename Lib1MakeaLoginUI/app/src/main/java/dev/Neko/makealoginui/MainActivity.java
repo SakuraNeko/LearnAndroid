758b0b3123ff615fdf399ef52a6d0c6ce6e46a7f
SakuraNeko <nekocn@outlook.com>
2020-10-06 21:27:39 +0800

    Lib1

---
+++ Lib1MakeaLoginUI/app/src/main/java/dev/Neko/makealoginui/MainActivity.java
@@ -0,0 +1,30 @@
+package dev.Neko.makealoginui;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        Button button=(Button) findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Main3Activity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+        ActionBar a=getSupportActionBar();
+        if(a!=null)
+        {
+            a.hide();
+        }
+    }
+}
